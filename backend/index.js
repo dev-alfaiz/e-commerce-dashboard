5ec3ea7ed77058c8b4496664d593818802ab59fd
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-18 12:57:23 +0530

    Implemented signup api

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
+require("./db/config");
+const User = require("./db/User");
 
 const app = express();
+app.use(express.json());
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  brand: String,
-  price: Number,
-  category: String,
+app.post("/register", async (request, response) => {
+  const user = new User(request.body);
+  const result = await user.save();
+  response.send(result);
 });
 
-const product = mongoose.model("products", productSchema);
-
-const connectDB = async () => {
-  mongoose.connect("mongodb://localhost:27017/e-comm");
-  const data = await product.find();
-  console.warn(data);
-};
-connectDB()
-
-app.get("/", (request, response) => {
-  response.send("API is working.");
-});
-
-app.listen(5051);
+app.listen(5050);
