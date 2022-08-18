b0d47f146c69bf13e3b7d52d5ec4fde2a890408a
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-18 11:43:15 +0530

    Connect with db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  brand: String,
+  price: Number,
+  category: String,
+});
+
+const product = mongoose.model("products", productSchema);
+
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/e-comm");
+  const data = await product.find();
+  console.warn(data);
+};
+connectDB()
 
 app.get("/", (request, response) => {
   response.send("API is working.");
 });
 
-app.listen(5050)
+app.listen(5051);
