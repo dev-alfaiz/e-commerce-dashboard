8c5a8f4b864fb35d0e91928fe41b59a54debdb42
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-21 07:31:53 +0530

    implemented login api in backend

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,24 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (request, response) => {
-  const user = new User(request.body);
-  const result = await user.save();
+  let user = new User(request.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   response.send(result);
+});
+
+app.post("/login", async (request, response) => {
+  if (request.body.email && request.body.password) {
+    let user = await User.findOne(request.body).select("-password");
+    if (user) {
+      response.send(user);
+    } else {
+      response.send({ result: "No user found." });
+    }
+  } else {
+    response.send({ result: "No user found." });
+  }
 });
 
 app.listen(5050);
